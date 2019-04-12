e440638713fa2c5aa11d06d23f8293c1fb0a4473
feeewq3 <16137238uca>
2019-04-12 08:55:42 -0600

    landskey

--- app/src/main/java/com/example/pdm_00002517_laboratorio_05/MainActivity.kt
+++ app/src/main/java/com/example/pdm_00002517_laboratorio_05/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.pdm_00002517_laboratorio_05
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 
 //clase abstracta metodos definidos
 class MainActivity : AppCompatActivity(), Fragment_basic.ToolBoxListener{
@@ -15,6 +17,9 @@ class MainActivity : AppCompatActivity(), Fragment_basic.ToolBoxListener{
 
         supportFragmentManager.beginTransaction().add(R.id.activity_content, fragmentBasic)
                 .commit()
+        if (resources.configuration.orientation==Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this,"Estoy en landskey",Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun ClickListenerHandler(view: TextView) {
